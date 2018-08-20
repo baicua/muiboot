d0108b147bcacbe192537094dd9ed970b356f601
yangjin <yangjin1@lvmama.com>
2018-08-20 14:47:36 +0800

    修改项目名称

--- baicua-shiro/src/main/java/com/baicua/shiro/system/dao/RoleMenuMapper.java
+++
@@ -1,7 +0,0 @@
-package com.baicua.shiro.system.dao;
-
-import com.baicua.shiro.common.config.MyMapper;
-import com.baicua.shiro.system.domain.RoleMenu;
-
-public interface RoleMenuMapper extends MyMapper<RoleMenu> {
-}
