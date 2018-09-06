b0035d54c95d1323d6f6c200a466dcb12bcbcf49
yangjin <yangjin1@lvmama.com>
2018-09-06 18:47:14 +0800

    日常开发

--- muiboot-shiro/src/main/java/com/muiboot/shiro/route/ModelController.java
+++ muiboot-shiro/src/main/java/com/muiboot/shiro/route/ModelController.java
@@ -2,12 +2,9 @@ package com.muiboot.shiro.route;
 
 import com.muiboot.shiro.common.controller.BaseController;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
