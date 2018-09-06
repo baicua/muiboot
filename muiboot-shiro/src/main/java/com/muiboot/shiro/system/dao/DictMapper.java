8a2fd8742a0fc36ed4a7bd1521c96d4bdc7d6fcf
yangjin <yangjin1@lvmama.com>
2018-09-06 15:08:59 +0800

    日常开发

--- muiboot-shiro/src/main/java/com/muiboot/shiro/system/dao/DictMapper.java
+++
@@ -1,7 +0,0 @@
-package com.muiboot.shiro.system.dao;
-
-import com.muiboot.shiro.common.config.MyMapper;
-import com.muiboot.shiro.system.domain.Dict;
-
-public interface DictMapper extends MyMapper<Dict> {
-}
