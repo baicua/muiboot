dddfa0e08f998446f964a074601cf326791473a2
杨进 <17612150729@163.com>
2018-10-04 11:15:55 +0800

    日常开发

--- muiboot-shiro/src/main/java/com/muiboot/shiro/common/dao/GenerateMapper.java
+++
@@ -1,7 +0,0 @@
-package com.muiboot.shiro.common.dao;
-
-import com.muiboot.shiro.common.config.MyMapper;
-import com.muiboot.shiro.common.domain.Generate;
-
-public interface GenerateMapper extends MyMapper<Generate> {
-}
