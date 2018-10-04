b4010c235ca5ab7fdf10949042e62ae40a44af45
杨进 <17612150729@163.com>
2018-10-04 22:57:17 +0800

    日常开发

---
+++ muiboot-shiro/src/main/java/com/muiboot/shiro/system/controller/SysConstant.java
@@ -0,0 +1,9 @@
+package com.muiboot.shiro.system.controller;
+
+/**
+ * Created by 75631 on 2018/10/4.
+ */
+public interface SysConstant {
+    public static final Long BASE_ROLE_KEY=80L;//用户初始角色
+    public static final String INIT_USER_PWD="111111";//用户初始角色
+}
