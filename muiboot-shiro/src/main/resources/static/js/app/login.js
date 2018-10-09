b4040bd33989baa660baa012fb3206545ba60222
杨进 <17612150729@163.com>
2018-10-10 00:33:30 +0800

    文档系统加拦截器

--- muiboot-shiro/src/main/resources/static/js/app/login.js
+++ muiboot-shiro/src/main/resources/static/js/app/login.js
@@ -74,16 +74,24 @@ function reloadCode() {
 function addClass(obj, cls){
     var obj_class = obj.className,//获取 class 内容.
         blank = (obj_class != '') ? ' ' : '';//判断获取到的 class 是否为空, 如果不为空在前面加个'空格'.
-    added = obj_class + blank + cls;//组合原来的 class 和需要添加的 class.
+    if(obj_class.indexOf(cls)>-1){
+        return;
+    }
+    var added = trim(obj_class) + blank + cls;//组合原来的 class 和需要添加的 class.
     obj.className = added;//替换原来的 class.
 }
 function removeClass(obj, cls){
-    var obj_class = ' '+obj.className+' ';//获取 class 内容, 并在首尾各加一个空格. ex) 'abc    bcd' -> ' abc    bcd '
-    obj_class = obj_class.replace(/(\s+)/gi, ' '),//将多余的空字符替换成一个空格. ex) ' abc    bcd ' -> ' abc bcd '
-        removed = obj_class.replace(' '+cls+' ', ' ');//在原来的 class 替换掉首尾加了空格的 class. ex) ' abc bcd ' -> 'bcd '
-    removed = removed.replace(/(^\s+)|(\s+$)/g, '');//去掉首尾空格. ex) 'bcd ' -> 'bcd'
-    obj.className = removed;//替换原来的 class.
+    var obj_class =obj.className;
+    if(obj_class){
+        obj.className=trim(obj_class.replace(cls,' '));
+    }
 }
+//去掉前后空格
+function trim(str)
+{
+    return str.replace(/(^\s*)|(\s*$)/g, "");
+}
+
 //创建ajax函数
 function ajax(options){
     options=options||{};
