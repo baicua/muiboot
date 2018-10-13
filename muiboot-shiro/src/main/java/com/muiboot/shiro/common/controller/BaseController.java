80c52f708a56ca7a0da995b3ba6d56af3362a14c
杨进 <17612150729@163.com>
2018-10-14 00:21:34 +0800

    日常开发

--- muiboot-shiro/src/main/java/com/muiboot/shiro/common/controller/BaseController.java
+++ muiboot-shiro/src/main/java/com/muiboot/shiro/common/controller/BaseController.java
@@ -14,6 +14,7 @@ import com.github.pagehelper.PageInfo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
@@ -26,6 +27,7 @@ import javax.servlet.http.HttpServletResponse;
 * @version 1.0 2018/10/12
 * @author jin
 */
+@Controller
 public  abstract class BaseController {
 	protected  final Logger logger = LoggerFactory.getLogger(this.getClass());
 	@ModelAttribute
--- muiboot-shiro/src/main/java/com/muiboot/shiro/system/controller/SysConstant.java
+++ muiboot-shiro/src/main/java/com/muiboot/shiro/system/controller/SysConstant.java
@@ -9,6 +9,6 @@ public interface SysConstant {
     public static final Long BASE_ROLE_KEY=80L;//用户初始角色
     public static final String INIT_USER_PWD="111111";//用户初始角色
     public static final long PAGE_MAX_AGE = TimeUnit.DAYS.toSeconds(7);//页面缓存时间-缓存7天
-    public static final long DATA_MAX_AGE =5;//数据缓存时间-缓存5秒
+    public static final long DATA_MAX_AGE =3;//数据缓存时间-缓存3秒
     public static final long START_TIME =System.currentTimeMillis();//项目启动时间
 }
