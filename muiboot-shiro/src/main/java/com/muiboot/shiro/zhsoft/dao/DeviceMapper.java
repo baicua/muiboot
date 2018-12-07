def41ad513223cf48c61084dae1a11f25c17aff7
yangjin <yangjin1@lvmama.com>
2018-12-07 15:23:49 +0800

    删除

--- muiboot-shiro/src/main/java/com/muiboot/shiro/zhsoft/dao/DeviceMapper.java
+++
@@ -1,7 +0,0 @@
-package com.muiboot.shiro.zhsoft.dao;
-
-import com.muiboot.shiro.common.config.MyMapper;
-import com.muiboot.shiro.zhsoft.domain.Device;
-
-public interface DeviceMapper extends MyMapper<Device> {
-}
