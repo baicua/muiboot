2ecc4d0482f478abfe9ec3ff622bcf54f2a3768b
yangjin <yangjin1@lvmama.com>
2018-12-07 15:23:03 +0800

    删除

--- muiboot-shiro/src/main/java/com/muiboot/shiro/zhsoft/dao/UserWeichatMapper.java
+++
@@ -1,7 +0,0 @@
-package com.muiboot.shiro.zhsoft.dao;
-
-import com.muiboot.shiro.common.config.MyMapper;
-import com.muiboot.shiro.zhsoft.domain.UserWeichat;
-
-public interface UserWeichatMapper extends MyMapper<UserWeichat> {
-}
