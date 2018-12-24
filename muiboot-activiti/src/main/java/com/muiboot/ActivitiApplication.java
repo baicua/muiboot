bffa5d84d8da5c594b117af83072d40b42043e9c
杨进 <17612150729@163.com>
2018-12-25 01:10:01 +0800

    日常开发

--- muiboot-activiti/src/main/java/com/muiboot/ActivitiApplication.java
+++ muiboot-activiti/src/main/java/com/muiboot/ActivitiApplication.java
@@ -1,8 +1,5 @@
 package com.muiboot;
 
-import org.activiti.engine.*;
-import org.activiti.spring.ProcessEngineFactoryBean;
-import org.activiti.spring.SpringProcessEngineConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.mybatis.spring.annotation.MapperScan;
@@ -11,14 +8,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @EnableCaching
 @SpringBootApplication
-@MapperScan("com.muiboot.**.dao")
 @EnableTransactionManagement
 @EnableAutoConfiguration(exclude={
 		JpaRepositoriesAutoConfiguration.class//禁止springboot自动加载持久化bean
